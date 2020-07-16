1279fdc10e842a836b23003d4db8402c1e77e9a3
liukun-uzi <1252740481@qq.com>
2020-07-16 10:03:58 +0800

    修改

---
+++ src/network/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export function request(config) {
+    
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:8000/api/n3',
+    timeout: 5000
+  })
+
+  //2.axios拦截器
+  axios.interceptors.request.use(config => {
+  return config
+  },err => {
+
+  })
+  //响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  },err => {
+    console.log(err);
+  })
+  //3.发送真正的网络请求
+  return instance(config)
+}
