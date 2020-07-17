5637ebaed0de53e2c3d3e771ac394854a032c547
liukun-uzi <1252740481@qq.com>
2020-07-17 16:14:18 +0800

    修改

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,130 @@
 <template>
-  <h2>分类</h2>
+  <div class="wrapper">
+    <ul class="content">
+      <li>1hahahah</li>
+      <li>2hahahah</li>
+      <li>3hahahah</li>
+      <li>4hahahah</li>
+      <li>5hahahah</li>
+      <li>6hahahah</li>
+      <li>7hahahah</li>
+      <li>8hahahah</li>
+      <li>9hahahah</li>
+      <li>10hahahah</li>
+      <li>11hahahah</li>
+      <li>12hahahah</li>
+      <li>13hahahah</li>
+      <li>14hahahah</li>
+      <li>15hahahah</li>
+      <li>16hahahah</li>
+      <li>17hahahah</li>
+      <li>18hahahah</li>
+      <li>19hahahah</li>
+      <li>20hahahah</li>
+      <li>21hahahah</li>
+      <li>22hahahah</li>
+      <li>23hahahah</li>
+      <li>24hahahah</li>
+      <li>25hahahah</li>
+      <li>26hahahah</li>
+      <li>27hahahah</li>
+      <li>28hahahah</li>
+      <li>29hahahah</li>
+      <li>30hahahah</li>
+      <li>31hahahah</li>
+      <li>32hahahah</li>
+      <li>33hahahah</li>
+      <li>34hahahah</li>
+      <li>35hahahah</li>
+      <li>36hahahah</li>
+      <li>37hahahah</li>
+      <li>38hahahah</li>
+      <li>39hahahah</li>
+      <li>40hahahah</li>
+      <li>41hahahah</li>
+      <li>42hahahah</li>
+      <li>43hahahah</li>
+      <li>44hahahah</li>
+      <li>45hahahah</li>
+      <li>46hahahah</li>
+      <li>47hahahah</li>
+      <li>48hahahah</li>
+      <li>49hahahah</li>
+      <li>50hahahah</li>
+      <li>51hahahah</li>
+      <li>52hahahah</li>
+      <li>53hahahah</li>
+      <li>54hahahah</li>
+      <li>55hahahah</li>
+      <li>56hahahah</li>
+      <li>57hahahah</li>
+      <li>58hahahah</li>
+      <li>59hahahah</li>
+      <li>60hahahah</li>
+      <li>61hahahah</li>
+      <li>62hahahah</li>
+      <li>63hahahah</li>
+      <li>64hahahah</li>
+      <li>65hahahah</li>
+      <li>66hahahah</li>
+      <li>67hahahah</li>
+      <li>68hahahah</li>
+      <li>69hahahah</li>
+      <li>70hahahah</li>
+      <li>71hahahah</li>
+      <li>72hahahah</li>
+      <li>73hahahah</li>
+      <li>74hahahah</li>
+      <li>75hahahah</li>
+      <li>76hahahah</li>
+      <li>77hahahah</li>
+      <li>78hahahah</li>
+      <li>79hahahah</li>
+      <li>80hahahah</li>
+      <li>81hahahah</li>
+      <li>82hahahah</li>
+      <li>83hahahah</li>
+      <li>84hahahah</li>
+      <li>85hahahah</li>
+      <li>86hahahah</li>
+      <li>87hahahah</li>
+      <li>88hahahah</li>
+      <li>89hahahah</li>
+      <li>90hahahah</li>
+      <li>91hahahah</li>
+      <li>92hahahah</li>
+      <li>93hahahah</li>
+      <li>94hahahah</li>
+      <li>95hahahah</li>
+      <li>96hahahah</li>
+      <li>97hahahah</li>
+      <li>98hahahah</li>
+      <li>99hahahah</li>
+      <li>100hahahah</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import BScroll from '@better-scroll/core'
   export default {
-    name: "Category"
+    name: "Category",
+    created() {
+      
+    },
+    mounted() {
+      let BS = new BScroll('.wrapper', {
+
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .wrapper {
+    height: 150px;
+    background-color:#ccc;
+    overflow: hidden;
+  }
+  
 </style>
